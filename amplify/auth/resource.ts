f34aeb7f8e04542ca008ad5e6b505c70cd1e6bd1
ivp0010 <izaiahperry@my.unt.edu>
2026-04-19 18:35:19 -0500

    step 3 and 4

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,17 +1,10 @@
 import { defineAuth } from '@aws-amplify/backend';
-
-
-import type { PostConfirmationTriggerHandler } from "aws-lambda"; 
-export const handler: PostConfirmationTriggerHandler = async (event) => { 
-  return event;
-};
-
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
+import { postConfirmation } from './post-confirmation/resource';
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  triggers: {
+    postConfirmation
+  }
 });
